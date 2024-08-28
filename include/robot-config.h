51c7d96f8c721b0a2de464e4da16b63939d81542
GoblinSharks <101782320+Gob1inSharks@users.noreply.github.com>
2024-08-28 09:50:41 -0400

    first push

---
+++ include/robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+
+//To set up a motor called LeftFront here, you'd use
+//extern motor LeftFront;
+
+extern motor LF;
+extern motor LM;
+extern motor LB;
+
+extern motor RF;
+extern motor RM;
+extern motor RB;
+
+extern motor IN;
+extern motor LIFT;
+
+//Add your devices below, and don't forget to do the same in robot-config.cpp:
+
+extern controller Controller;
+
+void  vexcodeInit( void );
