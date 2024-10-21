105f24925dbd3b8db9a51cc38dab327f1844eaba
GoblinSharks <101782320+Gob1inSharks@users.noreply.github.com>
2024-10-21 04:33:37 -0400

    Update usercontrol.cpp

--- src/usercontrol.cpp
+++ src/usercontrol.cpp
@@ -1,16 +1,42 @@
 #include "vex.h"
 
-bool 
+bool clampDown = false;
 
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
 
+    chassis.control_arcade(); //look in JAR-Template/drive.h for more info
 
+    //intake + hook HOLD DOWN
+    if(Controller1.ButtonR1.pressing()) {
+      IN.spin(forward, 127, pct);
+      HK.spin(forward, 127, pct);
+    } else if(Controller1.ButtonR2.pressing()) {
+      IN.spin(reverse, 127, pct);
+      HK.spin(reverse, 127, pct);
+    } else {
+      IN.stop(hold);
+      HK.stop(hold);
+    }
 
-    controller1.ButtonA
+    //clamp TOGGLE
+    if (Controller1.ButtonA.pressing()) {
+      if (clampDown) {
+        clampDown = false;
+      } else {
+        clampDown = true;
+      }
+    }
+    if (clampDown) {
+      CL.set(true);
+      wait(80,msec);
+    } else {
+      CL.set(false);
+      wait(80,msec);
+    }
 
-    chassis.control_arcade();
+    //lift TODO after rotation is installed
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted memory
