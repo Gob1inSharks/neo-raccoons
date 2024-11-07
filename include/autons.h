a203d32c39a13328f31938d86ee637dff1f2ef75
Goblin Sharks <101782320+Gob1inSharks@users.noreply.github.com>
2024-11-07 15:48:09 +0800

    update constants and autons

--- include/autons.h
+++ include/autons.h
@@ -9,9 +9,11 @@ void default_constants();
 
 void drive_test();
 void turn_test();
+void turnTest();
 void swing_test();
 void full_test();
 void odom_test();
 void tank_odom_test();
 void holonomic_odom_test();
 void gold_rush();
+void timed_rush();
--- src/autons.cpp
+++ src/autons.cpp
@@ -10,15 +10,64 @@
 
 void default_constants(){
   // Each constant set is in the form of (maxVoltage, kP, kI, kD, startI).
-  chassis.set_drive_constants(12, 1.5, 0, 10, 0);
-  chassis.set_heading_constants(6, .4, 0, 1, 0);
-  chassis.set_turn_constants(12, .4, .03, 3, 15);
-  chassis.set_swing_constants(12, .3, .001, 2, 15);
+  chassis.set_drive_constants(12, 1, .01, 2, 0);
+  chassis.set_heading_constants(6, .5, 1, 1, 1);
+  chassis.set_turn_constants(12, .5, .03, 4, 0);
+  chassis.set_swing_constants(12, .3, .001, 2, 0);
 
   // Each exit condition set is in the form of (settle_error, settle_time, timeout).
+  // Time is in miliseconds
   chassis.set_drive_exit_conditions(1.5, 300, 5000);
   chassis.set_turn_exit_conditions(1, 300, 3000);
   chassis.set_swing_exit_conditions(1, 300, 3000);
+}
+
+/**
+ * A little of this, a little of that; it should end roughly where it started.
+ */
+void full_test(){
+  chassis.drive_distance(24);
+  chassis.turn_to_angle(-45);
+  chassis.drive_distance(-36);
+  chassis.right_swing_to_angle(-90);
+  chassis.drive_distance(24);
+  chassis.turn_to_angle(0);
+}
+
+void calibrate(){
+  Brain.Screen.clearScreen();
+  Gyro.calibrate();
+  wait(5000,msec);
+  Brain.Screen.print("Inertial Calibrated");
+}
+
+void turnTest(){
+
+  calibrate();
+
+  float angle = 180;
+  float turn_max_voltage = 10;
+  float turn_settle_error = 3000;
+  float turn_settle_time = 3000;
+  float turn_timeout = 3000;
+
+  chassis.turn_to_angle(angle, turn_max_voltage);
+  while(1){
+    Brain.Screen.print(Gyro.angle());
+    Brain.Screen.newLine();
+    Brain.Screen.print(Gyro.installed());
+    Brain.Screen.newLine();
+    Brain.Screen.print(Gyro.heading());
+    wait(50,msec); //do not overwhelm the bot
+    Brain.Screen.clearScreen();
+  }
+}
+
+void timed_rush()
+{
+  //(float distance, float heading, float drive_max_voltage, float heading_max_voltage, float drive_settle_error, float drive_settle_time, float drive_timeout);
+  chassis.drive_distance(-25);
+  chassis.simpleTurnLeftforTime(600, 10);
 }
 
 /**
